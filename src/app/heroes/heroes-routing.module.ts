6d48ce59a81ba9481eeba077cc4e9903e48c9aea
daniel <daniel.romero48@uabc.edu.mx>
2023-02-08 16:29:18 -0800

    Rutas hijas

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       {
         path: 'listado',
@@ -23,11 +25,11 @@ const routes: Routes = [
         component: AgregarComponent
       },
       {
-        path: 'buscar/:id',
+        path: 'buscar',
         component: BuscarComponent
       },
       {
-        path: 'buscar',
+        path: ':id',
         component: HeroeComponent
       },
       {
